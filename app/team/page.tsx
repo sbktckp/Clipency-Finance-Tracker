9b3c1ca78f0de69ac9e054050c50aaffcfc81beb
Smit Bharat Patil <patilsmit2006@gmail.com>
2026-05-23 10:20:47 +0000

    Add Features, Engine and Team routes

---
+++ app/team/page.tsx
@@ -0,0 +1,9 @@
+import TeamSection from "@/components/landing/TeamSection"
+
+export default function TeamPage() {
+  return (
+    <main className="min-h-screen bg-black">
+      <TeamSection />
+    </main>
+  )
+}
